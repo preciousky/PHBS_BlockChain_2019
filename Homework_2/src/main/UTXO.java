3a06582f19a42882dcbeb9a68498aa7462031843
preciousky <1873685019@qq.com>
2019-10-20 21:39:47 +0800

    homework2

--- Homework_2/src/main/UTXO.java
+++ Homework_2/src/main/UTXO.java
@@ -4,13 +4,13 @@ import java.util.Arrays;
 
 public class UTXO implements Comparable<UTXO> {
 
-    // Hash of the transaction from which this main.UTXO originates
+    /** Hash of the transaction from which this main.UTXO originates */
     private byte[] txHash;
-    // Index of the corresponding output in said transaction
+
+    /** Index of the corresponding output in said transaction */
     private int index;
 
     /**
-     * constructor
      * Creates a new main.UTXO corresponding to the output with index <index> in the transaction whose
      * hash is {@code txHash}
      */
@@ -19,11 +19,19 @@ public class UTXO implements Comparable<UTXO> {
         this.index = index;
     }
 
+    /** @return the transaction hash of this main.UTXO */
+    public byte[] getTxHash() {
+        return txHash;
+    }
+
+    /** @return the index of this main.UTXO */
+    public int getIndex() {
+        return index;
+    }
+
     /**
      * Compares this main.UTXO to the one specified by {@code other}, considering them equal if they have
-     * {@code txHash} arrays with equal contents
-     * and
-     * equal {@code index} values
+     * {@code txHash} arrays with equal contents and equal {@code index} values
      */
     public boolean equals(Object other) {
         if (other == null) {
@@ -46,8 +54,8 @@ public class UTXO implements Comparable<UTXO> {
     }
 
     /**
-     * Simple implementation of a main.UTXO hashCode that respects equality of UTXOs
-     * i.e., utxo1.equals(utxo2)  =>  utxo1.hashCode() == utxo2.hashCode()
+     * Simple implementation of a main.UTXO hashCode that respects equality of UTXOs // (i.e.
+     * utxo1.equals(utxo2) => utxo1.hashCode() == utxo2.hashCode())
      */
     public int hashCode() {
         int hash = 1;
@@ -56,23 +64,15 @@ public class UTXO implements Comparable<UTXO> {
         return hash;
     }
 
-    /**
-     * Compares this main.UTXO to the one specified by {@code utxo}
-     *
-     * @param utxo another main.UTXO
-     * @return if the latter is greater return -1, if the former is greater, return 1, if they equals, return 0
-     */
+    /** Compares this main.UTXO to the one specified by {@code utxo} */
     public int compareTo(UTXO utxo) {
         byte[] hash = utxo.txHash;
         int in = utxo.index;
-        // if the index of the latter is greater, return -1
         if (in > index)
             return -1;
-            // if the index of the former is greater, return 1
         else if (in < index)
             return 1;
         else {
-            // if the txHash of the latter is greater, return -1
             int len1 = txHash.length;
             int len2 = hash.length;
             if (len2 > len1)
@@ -89,19 +89,5 @@ public class UTXO implements Comparable<UTXO> {
                 return 0;
             }
         }
-    }
-
-    /**
-     * @return the transaction hash of this main.UTXO
-     */
-    public byte[] getTxHash() {
-        return txHash;
-    }
-
-    /**
-     * @return the index of this main.UTXO
-     */
-    public int getIndex() {
-        return index;
     }
 }
